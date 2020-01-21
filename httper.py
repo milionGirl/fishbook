d38c53918e512388c2e14393a972656fb216b29f
bigGirl <825484254@qq.com>
2020-01-21 10:56:11 +0800

    尝试将视图与app分离开

---
+++ httper.py
@@ -0,0 +1,22 @@
+#urllib
+#requests
+import requests
+
+#scrapy并发多线程爬虫, requests + beautiful soap
+
+
+class HTTP:
+
+    @staticmethod
+    def get(url, return_json = True):
+        #r对象中包括http headers,大部分接口都是restful,
+        r = requests.get(url)
+        if r.status_code !=200:
+            return {} if return_json else ''
+        return r.json() if return_json else r.text
+        '''
+        if return_json:
+            return r.json()
+        else:
+            return r.text
+        '''
